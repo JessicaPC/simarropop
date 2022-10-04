6db219ca4b21d31b23ffa6b84a99fefa4c01a061
JessicaPC <jessicapicazocayuela@gmail.com>
2022-10-04 09:22:12 +0000

    proves git

---
+++ school/models/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class student(models.Model):
+     _name = 'school.student'
+     _description = 'The students'
+
+     name = fields.Char(string="Nombre", required=True)
+     year = fields.Integer()
+     topic_id = fields.Many2many("school.topic")
+    # topic_id = fields.Many2one("school.topic")
+
+
+class topic(models.Model):
+     _name = 'school.topic'
+     _description = 'The topics'
+     name = fields.Char(string="Topic name", required=True)
+     studens = fields.Many2many("school.student", "topic_id")
+
+class  course(models.Model):
+     _name = 'school.course'
+     _description = 'Courses'
+     name = fields.Char()
+     topics = fields.Many2many("school.topic")
+    # studens = fields.One2many("school.student", "topic_id") # necesita que hi haja un Many2one anteriorment
+  #   year = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
